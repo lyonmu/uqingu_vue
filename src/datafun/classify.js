b842c499fc428c8d20cdc1dfe4cb4c384b7a94ce
uqingu <uqingu@foxmail.com>
2022-05-03 23:00:01 +0800

    修改后端服务地址

--- src/datafun/classify.js
+++ src/datafun/classify.js
@@ -1,7 +1,7 @@
 // 获取数据的函数
 import axios from "axios";
 // 后端服务地址
-const SERVER_URl = "http://127.0.0.1:8001";
+const SERVER_URl = "http://192.168.0.103:8001";
 //分类信息相关
 export default {
     getClassifyList: async () => {
